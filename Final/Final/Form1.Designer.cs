b7857239aa10b716cd8bb18028682a0db642c96a
Birkew <mbj@kuglemus.dk>
2013-06-25 05:45:28 -0700

    ai

---
+++ Final/Final/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.frmFirstname = new System.Windows.Forms.TextBox();
+            this.frmLastname = new System.Windows.Forms.TextBox();
+            this.frmDescription = new System.Windows.Forms.TextBox();
+            this.frmEmail = new System.Windows.Forms.TextBox();
+            this.FrmLocation = new System.Windows.Forms.TextBox();
+            this.frmCompany = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(12, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Fornavn ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(12, 262);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Efternavn";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox3.ForeColor = System.Drawing.Color.Black;
+            this.textBox3.Location = new System.Drawing.Point(12, 288);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Description";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox4.ForeColor = System.Drawing.Color.Black;
+            this.textBox4.Location = new System.Drawing.Point(12, 314);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "E-mail";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox5.ForeColor = System.Drawing.Color.Black;
+            this.textBox5.Location = new System.Drawing.Point(12, 340);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Text = "Location";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.textBox6.ForeColor = System.Drawing.Color.Black;
+            this.textBox6.Location = new System.Drawing.Point(12, 366);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.Text = "Company";
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // frmFirstname
+            // 
+            this.frmFirstname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.frmFirstname.Location = new System.Drawing.Point(343, 236);
+            this.frmFirstname.Name = "frmFirstname";
+            this.frmFirstname.Size = new System.Drawing.Size(126, 20);
+            this.frmFirstname.TabIndex = 6;
+            // 
+            // frmLastname
+            // 
+            this.frmLastname.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.frmLastname.Location = new System.Drawing.Point(343, 262);
+            this.frmLastname.Name = "frmLastname";
+            this.frmLastname.Size = new System.Drawing.Size(126, 20);
+            this.frmLastname.TabIndex = 7;
+            // 
+            // frmDescription
+            // 
+            this.frmDescription.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.frmDescription.Location = new System.Drawing.Point(343, 288);
+            this.frmDescription.Name = "frmDescription";
+            this.frmDescription.Size = new System.Drawing.Size(126, 20);
+            this.frmDescription.TabIndex = 8;
+            // 
+            // frmEmail
+            // 
+            this.frmEmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.frmEmail.Location = new System.Drawing.Point(343, 314);
+            this.frmEmail.Name = "frmEmail";
+            this.frmEmail.Size = new System.Drawing.Size(126, 20);
+            this.frmEmail.TabIndex = 9;
+            // 
+            // FrmLocation
+            // 
+            this.FrmLocation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.FrmLocation.Location = new System.Drawing.Point(343, 340);
+            this.FrmLocation.Name = "FrmLocation";
+            this.FrmLocation.Size = new System.Drawing.Size(126, 20);
+            this.FrmLocation.TabIndex = 10;
+            // 
+            // frmCompany
+            // 
+            this.frmCompany.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.frmCompany.Location = new System.Drawing.Point(343, 366);
+            this.frmCompany.Name = "frmCompany";
+            this.frmCompany.Size = new System.Drawing.Size(125, 20);
+            this.frmCompany.TabIndex = 11;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 122);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(43, 20);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "Vis";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Location = new System.Drawing.Point(11, -1);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(386, 117);
+            this.listView1.TabIndex = 19;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 0;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.DisplayIndex = 2;
+            this.columnHeader2.Text = "Status";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.DisplayIndex = 1;
+            this.columnHeader3.Text = "Name";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "DisplayName";
+            this.columnHeader4.Width = 200;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 20);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Ryd";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.BackgroundImage = global::Final.Properties.Resources._1246702091a49f6h;
+            this.ClientSize = new System.Drawing.Size(480, 394);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.frmCompany);
+            this.Controls.Add(this.FrmLocation);
+            this.Controls.Add(this.frmEmail);
+            this.Controls.Add(this.frmDescription);
+            this.Controls.Add(this.frmLastname);
+            this.Controls.Add(this.frmFirstname);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Add Active Directory user\'s";
+            this.TextChanged += new System.EventHandler(this.f);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox frmFirstname;
+        private System.Windows.Forms.TextBox frmLastname;
+        private System.Windows.Forms.TextBox frmDescription;
+        private System.Windows.Forms.TextBox frmEmail;
+        private System.Windows.Forms.TextBox FrmLocation;
+        private System.Windows.Forms.TextBox frmCompany;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Final/Final/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+using System.Collections.ObjectModel;
+using Microsoft.PowerShell;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections;
+
+namespace Final
+{
+    public partial class Form1 : Form
+    {
+        private ListViewColumnSorter dinkat;
+        public Form1()
+        {
+            InitializeComponent();
+            dinkat = new ListViewColumnSorter();
+            listView1.ListViewItemSorter = dinkat;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            frmDescription.Text = textBox3.Text;
+        }
+        public void test()
+        {
+
+            PowerShell ps = PowerShell.Create();
+            using (Runspace rs = RunspaceFactory.CreateRunspace())
+            {
+                rs.Open();
+                ps.Runspace = rs;
+                Pipeline pip = rs.CreatePipeline();
+                pip.Commands.AddScript("get-service");
+                //pip.Commands.Add("Format-List");
+                Collection<PSObject> obj = pip.Invoke();
+                rs.Close();
+                string dinmor = "";
+                foreach (PSObject ting in obj)
+                {
+                    var fff = ting.BaseObject;
+                    
+                     ListViewItem dinhest = new ListViewItem();
+                     dinhest.SubItems.Add(ting.Properties["Status"].Value.ToString());
+                    dinhest.SubItems.Add(ting.Properties["Name"].Value.ToString());
+                    
+                    dinhest.SubItems.Add(ting.Properties["DisplayName"].Value.ToString());
+                    
+                    listView1.Items.Add(dinhest);
+                    //listView1.Items.Add(
+                    //textBox7.AppendText(ting.Properties["DisplayName"].Value.ToString() + "\r\n");
+                    
+                }
+                //MessageBox.Show(dinmor);
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(frmFirstname.Text + " " + frmLastname.Text);
+            test();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            // Determine if clicked column is already the column that is being sorted.
+            if (e.Column == dinkat.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                if (dinkat.Order == SortOrder.Ascending)
+                {
+                    dinkat.Order = SortOrder.Descending;
+                }
+                else
+                {
+                    dinkat.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                dinkat.SortColumn = e.Column;
+                dinkat.Order = SortOrder.Ascending;
+            }
+
+            // Perform the sort with these new sort options.
+            this.listView1.Sort();
+
+        }
+
+        private void f(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
